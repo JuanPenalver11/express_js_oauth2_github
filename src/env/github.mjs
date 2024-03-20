54faa9545f108b800e1f804bbc7cd28e130c76ba
juan penalver <juanpenalver@juans-Laptop.local>
2024-03-20 16:19:54 +0700

    some changes

--- src/env/github.mjs
+++ src/env/github.mjs
@@ -1,3 +1,3 @@
 
-export const CLIENTID = '7eee94c537cebba7b98f';
-export const CLIENTSECRET = 'a2f1c1b9c5b4c683d32ac807156402b4cd7976ea'
+export const CLIENTID = kkkkk;
+export const CLIENTSECRET = lllll
